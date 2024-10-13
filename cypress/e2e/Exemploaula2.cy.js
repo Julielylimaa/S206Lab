abb68f4e413a41bf819b04ab0ac51aead55435d1
Juliely <juhlima26@icloud.com>
2024-10-13 20:02:28 -0300

    feat: :sparkles: add delete test

--- cypress/e2e/Exemploaula2.cy.js
+++ cypress/e2e/Exemploaula2.cy.js
@@ -29,6 +29,26 @@ describe("Testes da criação, registro e login", ()=> {
     cy.get('h1.ng-binding').should("contain.text", ID)
   })
 
+  it("Teste de deletar usuário com sucesso", () => {
+    cy.visit("https://www.globalsqa.com/angularJs-protractor/registration-login-example/#/login")
+    let [ID] = loginUser()
+    cy.get('ul.ng-scope').should("contain.text", ID)
+    cy.get(':nth-child(1) > a').click()
+    cy.get('ul.ng-scope').should("not.contain.text", ID)
+
+  })
+  it("Teste de deletar usuário com falha", () => {
+    cy.visit("https://www.globalsqa.com/angularJs-protractor/registration-login-example/#/login")
+    let [ID, Senha] = loginUser()
+    cy.get('ul.ng-scope').should("contain.text", ID)
+    cy.get(':nth-child(1) > a').click()
+    cy.get('.btn').click()
+    cy.get('#username').type(ID)
+    cy.get('#password').type(Senha)
+    cy.get('.btn-primary').click()
+    cy.get('.ng-binding').should("contain.text", "incorrect")
+  })
+
 })
 
 function criarUser(){
@@ -51,3 +71,13 @@ function criarUser(){
   return infos
 }
 
+
+function loginUser(){
+  let [ID, Senha] = criarUser()
+    cy.visit("https://www.globalsqa.com/angularJs-protractor/registration-login-example/#/login")
+    cy.get('#username').type(ID)
+    cy.get('#password').type(Senha)
+    cy.get('.btn-primary').click()
+    cy.get('h1.ng-binding').should("contain.text", ID)
+    return ID, Senha
+}
